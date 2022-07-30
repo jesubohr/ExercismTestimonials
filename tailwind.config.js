3bb035830da980f1ac994c5c021ae875025f0ec8
Jesús Borrero <39781228+jesubohr@users.noreply.github.com>
2022-07-30 18:41:39 -0500

    Minor configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,8 +12,9 @@ module.exports = {
                 poppins: ['Poppins', 'sans-serif'],
             },
             boxShadow: {
-                'container': '0px 4px 42px 0px #4F72CD26',
-                'button': '0px 1px 0px 1px #CBC9D999',
+              'container': '0px 4px 42px 0px #4F72CD26',
+              'button': '0px 1px 0px 1px #CBC9D999',
+              'selector': '0px 0px 2px 2px #2E57E840',
             }
         }
     },
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import svgr from 'vite-plugin-svgr'
 import react from '@vitejs/plugin-react'
+const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()]
+  plugins: [react(), svgr()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    }
+  }
 })
