6811947f6a418790de6845b7ac5ef9dd2074dc97
Jesús Borrero <39781228+jesubohr@users.noreply.github.com>
2022-09-12 02:03:50 -0500

    Small refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { Header } from "./components/Header";
-import { BannerTitle } from "./components/BannerTitle";
-import { Testimonials } from "./components/testimonials";
+import { Header } from "@/components/Header";
+import { BannerTitle } from "@/components/BannerTitle";
+import { Testimonials } from "@/components/testimonials";
 
 function App() {
   return (
     <>
       <Header />
-      <section className="flex flex-col items-center px-8 pb-10 h-[calc(100vh-4rem)]">
+      <section className="flex flex-col items-center px-8 pb-10">
         <BannerTitle />
         <Testimonials />
       </section>
