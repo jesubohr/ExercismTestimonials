036d54fd9925d0249e13a767514810ab0dc2b1bb
Jesús Borrero <39781228+jesubohr@users.noreply.github.com>
2022-09-12 01:54:25 -0500

    Added icons and styles in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,35 @@
 module.exports = {
-    content: ['./index.html', './src/**/*.tsx'],
-    theme: {
-        extend: {
-            colors: {
-                'label-default': '#130B43',
-                'label-secondary': '#5C5589',
-                'label-tertiary': '#76709F',
-                'light-border': '#D5D8E4',
-            },
-            fontFamily: {
-                poppins: ['Poppins', 'sans-serif'],
-            },
-            boxShadow: {
-              'container': '0px 4px 42px 0px #4F72CD26',
-              'button': '0px 1px 0px 1px #CBC9D999',
-              'selector': '0px 0px 2px 2px #2E57E840',
-            }
-        }
-    },
+  content: ['./index.html', './src/**/*.tsx'],
+  theme: {
+    extend: {
+      width:{
+        '94': '23.5rem',
+      },
+      height: {
+        '91': '22.75rem',
+      },
+      maxHeight: {
+        '91': '22.75rem',
+      },
+      colors: {
+        'label-default': '#130B43',
+        'label-secondary': '#5C5589',
+        'label-tertiary': '#76709F',
+        'light-border': '#D5D8E4',
+      },
+      backgroundImage: {
+        'gradient': 'linear-gradient(180deg, #ffffff80 0%, #ffffff00 100%), #FFF4E3'
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+      boxShadow: {
+        'notification': '0px 4px 24px rgba(156, 130, 38, 0.4)',
+        'header-element': '0px 4px 16px 0px #4F72CD4D',
+        'container': '0px 4px 42px 0px #4F72CD26',
+        'button': '0px 1px 0px 1px #CBC9D999',
+        'selector': '0px 0px 2px 2px #2E57E840',
+      }
+    }
+  },
 }
