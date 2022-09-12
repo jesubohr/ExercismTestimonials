606b4f4fd6513000610c80455d869751d4145156
Jesús Borrero <39781228+jesubohr@users.noreply.github.com>
2022-09-12 01:57:03 -0500

    Update Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,9 @@ import { NavBar } from "./NavBar";
 export function Header() {
   return (
     <header className="flex items-center px-8 w-full h-16 border-b border-[#C8D5EF]">
-      <Logo className="h-6" />
+      <a href="https://exercism.org/" target="_blank">
+        <Logo className="h-6" />
+      </a>
       <nav className="flex justify-between items-center pr-3 w-full">
         <NavBar type="one" />
         <NavBar type="two" />
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,21 +6,29 @@ export function NavBar({ type }: { type: string }) {
   return (type === 'one')
     ? (
       <ul id="nav-one" className="flex items-center justify-between gap-8 pl-8">
-        <li className={`${NavOneItemStyles} gap-x-1.5 text-label-default`}>
-          <NavOne.DashboardIcon />
-          Dashboard
+        <li>
+          <a href="https://exercism.org/dashboard" className={`${NavOneItemStyles} gap-x-1.5 text-label-default`} target="_blank">
+            <NavOne.DashboardIcon />
+            Dashboard
+          </a>
         </li>
-        <li className={`${NavOneItemStyles}`}>
-          <NavOne.TracksIcon />
-          Tracks
+        <li>
+          <a href="https://exercism.org/tracks" className={NavOneItemStyles} target="_blank">
+            <NavOne.TracksIcon />
+            Tracks
+          </a>
         </li>
-        <li className={`${NavOneItemStyles}`}>
-          <NavOne.MentoringIcon />
-          Mentoring
+        <li>
+          <a href="https://exercism.org/mentoring" className={NavOneItemStyles} target="_blank">
+            <NavOne.MentoringIcon />
+            Mentoring
+          </a>
         </li>
-        <li className={`${NavOneItemStyles}`}>
-          <NavOne.ContributeIcon />
-          Contribute
+        <li>
+          <a href="https://exercism.org/contibuting" className={NavOneItemStyles} target="_blank">
+            <NavOne.ContributeIcon />
+            Contribute
+          </a>
         </li>
       </ul>
     )
@@ -32,16 +40,17 @@ export function NavBar({ type }: { type: string }) {
         <li className={NavTwoItemStyles}>
           <NavTwo.HexagonIcon />
         </li>
-        <li className={`${NavTwoItemStyles} -ml-3 pt-4`}>
-          <NavTwo.NotificationIcon />
+        <li className={`${NavTwoItemStyles} mr-5`}>
+          <button className="relative flex justify-center items-center py-2 w-[42px] h-9 bg-gradient rounded-lg text-[#E48900] shadow-notification">
+            <span className="absolute -top-3 -right-3 pt-[3px] w-6 h-6 bg-[#D85050] rounded-full text-[13px] text-white font-semibold">2</span>
+            <NavTwo.NotificationIcon  />
+          </button>
         </li>
         <li className={`${NavTwoItemStyles} -mt-2 -ml-3 mr-0.5`}>
           <NavTwo.BadgeIcon />
         </li>
-        <li className={NavTwoItemStyles}>
-          <NavTwo.ProfileIcon />
-        </li>
-        <li className={`${NavTwoItemStyles} -ml-5 p-1 rounded-full hover:bg-black/10`}>
+        <li className={`${NavTwoItemStyles} gap-5`}>
+          <NavTwo.ProfileIcon className="rounded-full shadow-header-element" />
           <NavTwo.MenuIcon />
         </li>
       </ul>
