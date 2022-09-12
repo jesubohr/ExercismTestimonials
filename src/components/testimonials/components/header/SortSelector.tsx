c4c5c0c13c9b858637f713b7f99096f79db1cf7f
Jesús Borrero <39781228+jesubohr@users.noreply.github.com>
2022-09-12 02:00:42 -0500

    Update SortSelector styles and format util

--- src/components/testimonials/components/header/SortSelector.tsx
+++ src/components/testimonials/components/header/SortSelector.tsx
@@ -1,20 +1,34 @@
 import { useState } from 'react'
 import { Listbox, Transition } from '@headlessui/react'
+import { useOptions } from '@/hooks/useTestimonialOption'
 
-const SORT_OPTIONS = ['Newest', 'Oldest']
+const SORT_OPTIONS = ['oldest_first', 'newest_first']
 export function SortSelector() {
   const [selectedOption, setSelectedOption] = useState(SORT_OPTIONS[0])
+  const { onSortChange } = useOptions()
+
+  const formatOption = (option: string) => {
+    return option
+      .split('_')
+      .map((word) => word[0].toUpperCase() + word.slice(1))
+      .join(' ')
+  }
+
+  function handleSortChange(option: string) {
+    setSelectedOption(option)
+    onSortChange(option)
+  }
 
   return (
     <Listbox
       as="div"
       value={selectedOption}
-      onChange={setSelectedOption}
-      className="group relative w-[21rem]"
+      onChange={handleSortChange}
+      className="group relative w-[21rem] z-10"
     >
       <Listbox.Button
       className="flex justify-between items-center py-3 px-5 w-full bg-[#F0F3F9] text-label-secondary rounded-md ring-1 ring-transparent group-focus:outline-none group-focus-within:bg-white group-focus-within:ring-[#2E57E8] group-focus-within:shadow-selector">
-        Sort by {selectedOption} First
+        Sort by {formatOption(selectedOption)}
         <SelectorIcon />
       </Listbox.Button>
       <Transition
@@ -25,7 +39,7 @@ export function SortSelector() {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Listbox.Options static className="absolute mt-3 p-2 max-h-60 w-full overflow-auto rounded-md bg-white text-base shadow-container sm:text-sm">
+        <Listbox.Options static className="absolute mt-3 p-2 max-h-60 w-full border-2 rounded-md bg-white text-base shadow-container overflow-auto sm:text-sm">
           {SORT_OPTIONS.map((option, index) => (
             <Listbox.Option
               key={index}
@@ -35,7 +49,7 @@ export function SortSelector() {
               }>
               {({ selected }) => (
                 <>
-                  {option}
+                  {formatOption(option)}
                   {selected && <CheckIcon />}
                 </>
               )}
